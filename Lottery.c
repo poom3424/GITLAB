161fe2933c17b315eea60b02ae715abf24682f15
ddon007 <dondawn37919@hotmail.com>
2020-10-20 16:24:16 +0700

    Modified Lottery.c

--- Lottery.c
+++ Lottery.c
@@ -8,7 +8,7 @@ void main()
   
   int YourLotteryNo;  
    
-  printf("Enter your Lottery Number (0 to end): "); 
+  printf("Enter your Lottery Number (0 to ending): "); 
   scanf("%d", &YourLotteryNo); 
   while (YourLotteryNo != 0) 
   { 
